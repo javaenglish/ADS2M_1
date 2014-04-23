323c479361ff9b975a5d242f7e8224be1372fc57
Tarcisio Monteiro <rafandai@gmail.com>
2014-04-23 11:15:08 -0300

    documentation for the main

--- Sistema Bancario/src/com/senac/bank/apps/System.java
+++ Sistema Bancario/src/com/senac/bank/apps/System.java
@@ -6,8 +6,10 @@ public class System {
 
 	public static void main(String[] args) {
 	
+		//creates the controller to run the application
 		BankController masterController = new BankController();
 		
+		//shows the option for the user
 		masterController.options();
 		
 		}
