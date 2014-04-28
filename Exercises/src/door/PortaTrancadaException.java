432b78f048c20bd26b9fb72a9c77affa9eeb044b
Tarcisio Monteiro <rafandai@gmail.com>
2014-04-28 10:31:39 -0300

    Package changing

--- Exercises/src/door/PortaTrancadaException.java
+++ Exercises/src/door/PortaTrancadaException.java
@@ -1,6 +1,9 @@
 package door;
 
 public class PortaTrancadaException extends Exception {
+	private static final long serialVersionUID = -2601056215007283087L;
 	
-
+	public PortaTrancadaException(){
+		super("The door is locked");
+	}
 }
