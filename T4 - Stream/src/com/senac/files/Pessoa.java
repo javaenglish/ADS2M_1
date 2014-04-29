69595975ed804d5f3b105742b36de81321333a02
Tarcisio Monteiro <rafandai@gmail.com>
2014-04-28 22:46:52 -0300

    Class Pessoa of the work wih documentation

--- T4 - Stream/src/com/senac/files/Pessoa.java
+++ T4 - Stream/src/com/senac/files/Pessoa.java
@@ -2,4 +2,37 @@ package com.senac.files;
 
 public class Pessoa {
 
+		private String name;
+		private String address;
+		private String phone[];
+		
+		//constructor with fields
+		public Pessoa(String nome, String address, String phone[]) 
+		{
+			this.name 				= nome;
+			this.address 			= address;
+			this.phone 			= phone;
+		}
+		
+		public String getName()
+		{
+			return name;
+		}
+		
+		public String getEndereco()
+		{
+			return address;
+		}
+		
+		public String[] getTelefone()
+		{
+			return phone;
+		}
+		
+		//sets the phone by an index
+		public void setTelefone(int index, String phone)
+		{
+			this.phone[index] = phone;
+	}
 }
+
