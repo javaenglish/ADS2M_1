680710b59b930130a91710d5db19887dd1702ff2
Tarcisio Monteiro <rafandai@gmail.com>
2014-05-14 11:09:02 -0300

    Broken Methods

--- GerenciadorDeFuncionarios/src/com/senac/gerenciador/model/Funcionario.java
+++ GerenciadorDeFuncionarios/src/com/senac/gerenciador/model/Funcionario.java
@@ -3,18 +3,30 @@ package com.senac.gerenciador.model;
 public class Funcionario {
 	
 	private static int PROXIMA_MATRICULA = 1;
-	private String cargo;
 	private double salario;
 		
 	private int matricula;
+	private Cargo cargo;
 	
-	public Funcionario() {
+	public Funcionario(Cargo cargo, double Salario) throws Exception {
+		
+		if (salario < cargo.getSalarioMinimo())
+			throw new Exception("Salario Minimo Invalido");
 		matricula = PROXIMA_MATRICULA++;
+		this.cargo = cargo;
+		this.salario = salario;
 	}
-
 
 	public int getMatricula() {
 		return 1;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
 	}
 	
 }
--- GerenciadorDeFuncionarios/test/com/senac/gerenciador/model/test/FuncionarioTest.java
+++ GerenciadorDeFuncionarios/test/com/senac/gerenciador/model/test/FuncionarioTest.java
@@ -4,22 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.senac.gerenciador.model.Cargo;
 import com.senac.gerenciador.model.Funcionario;
 
 public class FuncionarioTest {
+	
+	private Funcionario estagiario;
 
 	@Test
-	public void testMatriculaValida() {
-		Funcionario func = new Funcionario();
+	public void testMatriculaValida() throws Exception {
+		Funcionario func = new Funcionario(Cargo.ESTAGIARIO, Cargo.ESTAGIARIO.getSalarioMinimo());
 		//
 		assertTrue(func.getMatricula()>0);
 	}
 
 	@Test
-	public void testNaoExistemDuasMatriculasiguais(){
-		Funcionario f = new Funcionario();
-		Funcionario	f1 = new Funcionario();
-		Funcionario f2 = new Funcionario();
+	public void testNaoExistemDuasMatriculasiguais() throws Exception{
+		Funcionario f = new Funcionario(Cargo.ESTAGIARIO, Cargo.ESTAGIARIO.getSalarioMinimo());
+		Funcionario f1 = new Funcionario(Cargo.ESTAGIARIO, Cargo.ESTAGIARIO.getSalarioMinimo());
+		Funcionario f2 = new Funcionario(Cargo.ESTAGIARIO, Cargo.ESTAGIARIO.getSalarioMinimo());
 		
 		assertFalse(f.getMatricula() == f1.getMatricula());
 		assertFalse(f1.getMatricula() == f2.getMatricula());
@@ -27,9 +30,15 @@ public class FuncionarioTest {
 		
 	}
 	
+	@Test(expected=java.lang.Exception.class)
+	public void testNaoPermiteCriarObjetoComSalarioInvalido() throws Exception {
+		new Funcionario(Cargo.ESTAGIARIO, Cargo.ESTAGIARIO.getSalarioMinimo());
+}
+	
 	@Test
-	public void testCargoValido() {
-		
+	public void testaSalarioDentroDaLei() throws Exception {
+		Funcionario func = new Funcionario(Cargo.ESTAGIARIO, Cargo.ESTAGIARIO.getSalarioMinimo());
+		assertTrue(func.getSalario() >= func.getCargo().getSalarioMinimo());
 	}
 	
 }
